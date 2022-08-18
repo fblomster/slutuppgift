0c24639f2c0d7777d919294e555ea6fdb77e3ddb
frida <frida.blomster@ha.ax>
2022-08-18 13:00:02 +0300

    Bearer token added succesfully

--- src/assets/js/slutuppgift.js
+++ src/assets/js/slutuppgift.js
@@ -1,7 +1,8 @@
 addEventListener("DOMContentLoaded", init);
+//updatedMessages();
 
 function init() {
-  //loadMessages();
+  loadMessages();
   const form = document.querySelector('form[name="message-form"]');
   form.addEventListener("submit", (event) => {
     console.log(event);
@@ -15,9 +16,34 @@ async function loadMessages() {
   const request = await fetch(
     "https://ha-slutuppgift-chat-do.westling.workers.dev/api/messages",
     {
-      method: "POST",
+      method: "get",
       headers: {
-        Authorization: "token ${token}",
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=",
+      },
+    }
+  );
+
+  const response = await request.json();
+
+  console.log({
+    request,
+    response,
+  });
+}
+
+async function updatedMessages() {
+  const token =
+    "N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=";
+  const request = await fetch(
+    "https://ha-slutuppgift-chat-do.westling.workers.dev/api/messages/updated",
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=",
       },
     }
   );
